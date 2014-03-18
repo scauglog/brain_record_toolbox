c3c2b5615824acc3cd0d9b0d899ba696bdfb1229
scauglog <scauglog@gmail.com>
2014-03-18 18:20:46 +0100

    same template for each trial

--- script_r448_trial5-6-7_process_data.py
+++ script_r448_trial5-6-7_process_data.py
@@ -23,34 +23,75 @@ koho_col = 4
 koho_row = 8
 weight_count = a_spike + b_spike
 max_weight = spike_thresh * 2
-alpha = 0.1  #learning coef
-neighbor = 4  #number of neighbor to modified
-min_win = 2  #number of time a neuron should win to be a good neuron
-dist_thresh = 4  #distance from which it's acceptable to create a new class
+#learning coef
+alpha = 0.1
+#number of neighbor to modified
+neighbor = 4
+#number of time a neuron should win to be a good neuron
+min_win = 2
+#distance from which it's acceptable to create a new class
+dist_thresh = 4
 
 #cluster parameter
-min_clus_abs = 20;  #minimum cluster size (absolute value)
-min_clus_rel = 0.01;  #minimum cluster size (relative value)
+#minimum cluster size (absolute value)
+min_clus_abs = 20
+#minimum cluster size (relative value)
+min_clus_rel = 0.01
 
+#trial to analyse
+trials = range(5 - 1, 7)
+
+#where to store data
 record_data = {}
 
 print('### load signal ###')
 sp = sig_proc.Signal_processing(save_img, show, img_ext)
 
 #load signal and sampling frequency
-signals = sp.load_m(dir_name + 'cell_trial.mat', 't')  #load multichannel signal
-fs = float(sp.load_m(dir_name + 'fech.mat', 'sampFreq'))  #load sample frequency
+signals = sp.load_m(dir_name + 'cell_trial.mat', 't')
+fs = float(sp.load_m(dir_name + 'fech.mat', 'sampFreq'))
 
-for trial in range(5-1, 7):
-    print('---- trial: '+str(trial+1)+' ----')
+
+trial = trials[0]
+print('---- trial: ' + str(trial + 1) + ' ----')
+signal = np.transpose(signals[0][trial])
+fsignal = sp.signal_mc_filtering(signal, low_cut, high_cut, fs)
+
+#all_chan_koho = []
+#first we find templates using kohonen network
+all_chan_templates = []
+for chan in range(fsignal.shape[0]):
+    #chan = 13
+    print('\n\n--- processing chan : ' + str(chan + 1) + ' ---')
+    s = fsignal[chan]
+    spikes_values, spikes_time = sp.find_spikes(s, a_spike, b_spike, spike_thresh)
+    print('spikes found: ' + str(spikes_values.shape[0]))
+    spikes_values = sp.smooth_spikes(spikes_values, 3)
+    koho = sp.find_spike_template_kohonen(spikes_values, koho_col, koho_row, weight_count, max_weight, alpha,
+                                          neighbor, min_win, dist_thresh)
+    #keep best cluster aka groups
+    min_clus = max(min_clus_abs, min_clus_rel * spikes_values.shape[0])
+
+    #keep only groups that have more spike than min_clus
+    koho.evaluate_group(spikes_values, 2 * dist_thresh, min_clus)
+
+    #to call plot you should have call evaluate_group before
+    txt='_trial_' + str(trial) + '_channel_' + str(chan + 1)
+    koho.plot_groups_stat(txt)
+    koho.plot_spikes_classified(spikes_values, 20, 2 * dist_thresh, txt)
+
+    all_chan_templates.append(koho.groups)
+if show:
+    sp.show_plot()
+
+#class spikes for each trial
+for trial in trials:
+    print('---- trial: ' + str(trial + 1) + ' ----')
     signal = np.transpose(signals[0][trial])
     fsignal = sp.signal_mc_filtering(signal, low_cut, high_cut, fs)
 
     all_chan_spikes_values = []
     all_chan_spikes_times = []
-    all_chan_koho = []
-    all_chan_templates = []
-    all_chan_spikes_classes = []
     all_chan_clusters = []
     for chan in range(fsignal.shape[0]):
         #chan = 13
@@ -59,27 +100,15 @@ for trial in range(5-1, 7):
         spikes_values, spikes_time = sp.find_spikes(s, a_spike, b_spike, spike_thresh)
         print('spikes found: ' + str(spikes_values.shape[0]))
         spikes_values = sp.smooth_spikes(spikes_values, 3)
-        koho = sp.find_spike_template_kohonen(spikes_values, koho_col, koho_row, weight_count, max_weight, alpha,
-                                              neighbor,
-                                              min_win, dist_thresh)
         all_chan_spikes_values.append(spikes_values)
-        all_chan_koho.append(koho)
-        #keep best cluster aka groups
-        min_clus = max(min_clus_abs, min_clus_rel * spikes_values.shape[0])
-        koho.evaluate_group(spikes_values, 2 * dist_thresh,
-                            min_clus)  #keep only groups that have more spike than min_clus
 
-        koho.plot_groups_stat('_trial_'+str(trial)+'_channel_' + str(chan + 1))  #to call this you should have call evaluate_group before
-        koho.plot_spikes_classified(spikes_values, 20, 2 * dist_thresh, '_trial_'+str(trial)+'_channel_' + str(chan + 1))
-
-        all_chan_templates.append(koho.groups)
         #classify spike
         all_chan_clusters.append([])
-        for gpe in koho.groups:
+        for gpe in all_chan_templates[chan]:
             all_chan_clusters[chan].append(sig_proc.Spikes_cluster(gpe.template, gpe.number + 1))
         spikes_classes = sp.classify_spikes(spikes_values, spikes_time, all_chan_clusters[chan], 2 * dist_thresh)
 
-        all_chan_spikes_classes.append(spikes_classes)
+        #all_chan_spikes_classes.append(spikes_classes)
         all_chan_spikes_times.append(spikes_time)
         all_chan_spikes_values.append(spikes_values)
 
@@ -87,7 +116,7 @@ for trial in range(5-1, 7):
         sp.show_plot()
 
     record_data[trial] = {'spikes_values': all_chan_spikes_values, 'spikes_time': all_chan_spikes_times,
-                          'spikes_classes': all_chan_spikes_classes, 'clusters': all_chan_clusters}
+                          'clusters': all_chan_clusters}
 
 if save_obj:
     with open(dir_name + 'data_processed', 'wb') as my_file:
